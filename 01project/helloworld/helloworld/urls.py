4ab5e94ec668c6350715c2d5d6e342de926dbd94
Jethet <henriette.hettinga@gmail.com>
2019-07-29 17:03:58 +0200

    Correct error in value

--- 01project/helloworld/helloworld/urls.py
+++ 01project/helloworld/helloworld/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hola.urls')),
-    path('', include('namaste.urls')),
+    path('namaste/', include('namaste.urls')),
 
 ]
--- 01project/helloworld/namaste/views.py
+++ 01project/helloworld/namaste/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def homepage(request):
+def namaste(request):
     return HttpResponse('Namaste means Hello in India, just like Namaskar.')
