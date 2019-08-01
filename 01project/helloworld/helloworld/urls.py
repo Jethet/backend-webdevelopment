5bf18f254cf37fac038bfe44606f7b1582a17a82
Jethet <henriette.hettinga@gmail.com>
2019-08-01 15:35:41 +0200

    Add app to urls

--- 01project/helloworld/helloworld/urls.py
+++ 01project/helloworld/helloworld/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from hola import views
+from hola import views\
 from namaste import views
 
 urlpatterns = [
