636a4080f5a070638f17de87a88275bb090391e9
Jethet <henriette.hettinga@gmail.com>
2019-08-01 17:52:16 +0200

    Create view for homepage

--- 03project-Databases/commandr/cmdr/views.py
+++ 03project-Databases/commandr/cmdr/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Command
 
-# Create your views here.
+class HomePageView(ListView):
+    model = Command
+    template_name = 'home.html'
