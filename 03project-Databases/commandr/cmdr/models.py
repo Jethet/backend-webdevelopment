f1778882df08a857079a001480212822f5647ff8
Jethet <henriette.hettinga@gmail.com>
2019-08-04 18:21:55 +0200

    Add comments to models.py

--- 03project-Databases/commandr/cmdr/models.py
+++ 03project-Databases/commandr/cmdr/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Command(models.Model):
     text = models.TextField()
 
+# This gets the name of what is entered as 'Command' and shows it:
+
     def __str__(self):
         return self.text
