f43e9e95b5a70a47ac522388e4bd13d3acc8f05a
Jethet <henriette.hettinga@gmail.com>
2019-08-05 18:51:39 +0200

    Change order of items to last added

--- 05project-Videosite/videosite/videorequest/views.py
+++ 05project-Videosite/videosite/videorequest/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Video
 
 def index(request):
-    videos = Video.objects.order_by('date_added')
+    videos = Video.objects.order_by('-date_added')
     context = {'videos': videos}
     return render(request, 'videorequest/index.html', context)
 
