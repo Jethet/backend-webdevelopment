f668342c5f4d4d01ae2290aa0aae598feb0ea5a4
Jethet <henriette.hettinga@gmail.com>
2019-08-05 16:10:52 +0200

    Add url for detail page to home.html and test detail page ok.

--- 04project-Blog/website/articles/urls.py
+++ 04project-Blog/website/articles/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleListView.as_view(), name='home'),
-    path('article/<int:pk>', viewsArticleDetailView.as_view(), name='detail'),
+    path('article/<int:pk>', views.ArticleDetailView.as_view(), name='detail'),
 ]
