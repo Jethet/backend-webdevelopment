34d33119ce4f78e23f56cb52d36502527554d143
Jethet <henriette.hettinga@gmail.com>
2019-08-09 16:29:18 +0200

    Create view and template form for new article

--- 06project-CRUDblog/website/articles/views.py
+++ 06project-CRUDblog/website/articles/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView
 from . models import Article
+from django.views.generic.edit import CreateView
 
 
 class ArticleListView(ListView):
@@ -11,5 +12,7 @@ class ArticleDetailView(DetailView):
     template_name = 'detail.html'
     context_object_name = 'post'
 
-class ArticleCreateView(ListView):
-    template_name = 'home.html'
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = '__all__'
