e8986d7379ee4dc8e12496387bfc417aec334fd8
Jethet <henriette.hettinga@gmail.com>
2019-08-09 16:43:12 +0200

    Test create new article - OK

--- 06project-CRUDblog/website/articles/models.py
+++ 06project-CRUDblog/website/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -14,3 +15,6 @@ class Article(models.Model):
 # defined by its title:
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('home')
