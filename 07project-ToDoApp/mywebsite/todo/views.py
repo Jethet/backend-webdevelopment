1f8aa914bf52016c42ed35e85a08e71a219689e1
Jethet <henriette.hettinga@gmail.com>
2019-08-13 13:30:00 +0200

    Use todo form for index page

--- 07project-ToDoApp/mywebsite/todo/views.py
+++ 07project-ToDoApp/mywebsite/todo/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TodoForm
+from .models import Todo
 
 def index(request):
-    return render(request, 'todo/index.html')
+    mytodo = Todo.objects.order_by('id')
+
+    form = TodoForm()
+    context = {'mytodo': mytodo, 'form': form}
+
+    return render(request, 'todo/index.html', context)
